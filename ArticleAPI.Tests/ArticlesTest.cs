679c25188c3653068123284f8ec3e55ab24d3ba2
Emre Biber <emrebiber@gmail.com>
2019-07-25 12:13:32 +0300

    couple of test added

---
+++ ArticleAPI.Tests/ArticlesTest.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+using ArticleAPI.DAL.Repositories.Article;
+using Moq;
+using Xunit;
+
+namespace ArticleAPI.Tests
+{
+    public class ArticlesControllerTest
+    {
+        [Fact]
+        public void Should_Mock_Function_Return_Article()
+        {
+            //Arrange
+            var mock = new Mock<IArticleRepository>();
+            var expected = new DAL.Models.Article();
+
+            mock.Setup(x => x.GetArticleById(1)).Returns(expected);
+
+            //Act
+            var actual = mock.Object.GetArticleById(1);
+
+            //Assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void Should_Mock_Function_Return_AllArticles()
+        {
+            //Arrange
+            var mock = new Mock<IArticleRepository>();
+            var expected = new List<DAL.Models.Article>();
+
+            mock.Setup(x => x.GetAllArticles()).Returns(expected);
+
+            //Act
+            var actual = mock.Object.GetAllArticles();
+
+            //Assert
+            Assert.Equal(expected, actual);
+        }
+    }
+}
